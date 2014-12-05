334c508441faf096ff38bca9f8fb7fe560ac0352
Johannes Sartisohn <jsartisohn@gmail.com>
2014-12-05 03:26:14 +0100

    Fix Fixed the API access exception handling.

---
+++ src/kRPCCompatibility/Compatibility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KRPCCompatibility
+{
+  public class RemoteTech {
+    private static IRemoteTechAPI api_;
+    public static IRemoteTechAPI API {
+      get {
+        if (api_ == null) {
+          try {
+            api_ = new RemoteTechAvailable();
+          } catch (System.IO.FileNotFoundException) {
+            api_ = new RemoteTechNotInstalled();
+          }
+        }
+        return api_;
+      }
+    }
+
+    public interface IRemoteTechAPI {
+      bool HasFlightComputer(Guid id);
+      void AddSanctionedPilot(Guid id, Action<FlightCtrlState> autopilot);
+      void RemoveSanctionedPilot(Guid id, Action<FlightCtrlState> autopilot);
+      bool HasAnyConnection(Guid id);
+      bool HasConnectionToKSC(Guid id);
+      double GetShortestSignalDelay(Guid id);
+      double GetSignalDelayToKSC(Guid id);
+      double GetSignalDelayToSatellite(Guid a, Guid b);
+    }
+  }
+}
